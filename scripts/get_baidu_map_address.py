556d8ef97ef54901010eeca52017617e9e02f2f6
yangxin <1819351140@qq.com>
2024-10-16 17:23:48 +0800

    fix(news_spider):三个新闻采集代码修改 feat(get_baidu_map_address):新增百度地图地址采集

---
+++ scripts/get_baidu_map_address.py
@@ -0,0 +1,146 @@
+# -*- coding:UTF-8 -*-
+# @Time    : 2024/8/15 13:51
+# @Author  : yangxin
+# @Email   : yang1819351140@163.com
+# @IDE     : PyCharm
+# @File    : test2.py
+# @Project : pythonScript
+# @Software: PyCharm
+import requests
+import pymysql
+
+
+def get_address_info(address='重庆市两江新区鱼嘴镇长和路65号12-23'):
+    try:
+        # print(address)
+        # encoding:utf-8
+        # 根据您选择的AK已为您生成调用代码
+        # 检测到您当前的AK设置了IP白名单校验
+        # 您的IP白名单中的IP非公网IP，请设置为公网IP，否则将请求失败
+        # 请在IP地址为0.0.0.0/0 外网IP的计算发起请求，否则将请求失败
+
+        # 服务地址
+        host = "https://api.map.baidu.com"
+
+        # 接口地址
+        uri = "/place/v2/suggestion"
+
+        # 此处填写你在控制台-应用管理-创建应用后获取的AK
+        ak = "8ymvmahEPpZT3jNPxK3oBS31LJUQYuO8"
+
+        params = {
+            "query": address,
+            "region": address,
+            "city_limit": "true",
+            "output": "json",
+            "ak": ak,
+
+        }
+
+        response = requests.get(url=host + uri, params=params)
+        if response:
+            data_list = response.json().get('result', [])
+            if data_list:
+                for data in data_list:
+                    uid = data.get('uid', '')
+                    if uid:
+                        province = data.get('province', '')
+                        city = data.get('city', '')
+                        district = data.get('district', '')
+                        adcode = data.get('adcode', '')
+                        return f'{province}{city}{district}', adcode
+        return '', ''
+    except:
+        return '', ''
+
+
+def get_address_info1(address):
+    try:
+        cookies = {
+            'PSTM': '1717486581',
+            'BIDUPSID': '9C3C36BF544BED968833257BF4AB0604',
+            'BDUSS': 'ndXOFZqdGxET0M2cTRMV0lPU3VWa2ZRRmRWNWpnUUUxNWt6Zm5mYnBDYmtaWWRtRVFBQUFBJCQAAAAAAAAAAAEAAABJaj8heXgxODE5MzUxMTQwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAOTYX2bk2F9mc',
+            'BDUSS_BFESS': 'ndXOFZqdGxET0M2cTRMV0lPU3VWa2ZRRmRWNWpnUUUxNWt6Zm5mYnBDYmtaWWRtRVFBQUFBJCQAAAAAAAAAAAEAAABJaj8heXgxODE5MzUxMTQwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAOTYX2bk2F9mc',
+            'BAIDUID': 'F3EC97B6825FC9023AA6BFAF29C6FB7B:SL=0:NR=10:FG=1',
+            'H_WISE_SIDS': '60622_60677_60676_60672_60572',
+            'BAIDUID_BFESS': 'F3EC97B6825FC9023AA6BFAF29C6FB7B:SL=0:NR=10:FG=1',
+            'ZFY': 'FCwsZLvf1:Bu5AvtfizlhFBTnYVuHltyCav:Bs015:ASgc:C',
+            '__bid_n': '1917dcb1a5d843229a5c49',
+            'delPer': '0',
+            'routetype': 'bus',
+            'H_PS_PSSID': '60622_60677_60676_60672_60572_60684_60700',
+            'PSINO': '1',
+            'BA_HECTOR': '04ag2g2l80al2h8g202h8la42rr9nh1jd02bq1u',
+            'BDORZ': 'B490B5EBF6F3CD402E515D22BCDA1598',
+            'BCLID': '11718421405245721845',
+            'BCLID_BFESS': '11718421405245721845',
+            'BDSFRCVID': 'biPOJexroG3Qs37tFKW2MkFPPyx5pROTDYLEOwXPsp3LGJLVYZ59EG0PtHt5Ixub6j3eogKK0mOTHUkF_2uxOjjg8UtVJeC6EG0Ptf8g0M5',
+            'BDSFRCVID_BFESS': 'biPOJexroG3Qs37tFKW2MkFPPyx5pROTDYLEOwXPsp3LGJLVYZ59EG0PtHt5Ixub6j3eogKK0mOTHUkF_2uxOjjg8UtVJeC6EG0Ptf8g0M5',
+            'H_BDCLCKID_SF': 'tRAOoC--JKvqKRopMtOhq4tehHRkWT39WDTm_DoyBn8VHUjmQx452xufQR3TBh5DBe62-pPKKR7ADKoa5hCMKRLmDHrq3lTk3mkjbIbGfn02OPKz0TKVX-4syP4j2xRnWNTWKfA-b4ncjRcTehoM3xI8LNj405OTbIFO0KJDJCF5hDIwDjD-DjPVKgTa54cbb4o2WbCQKJTm8pcN2b5oQTtuKN-f-U5iQbruBhQxLPTWMtJ-XpOUWfAkXpJvQnJjt2JxaqRCyxFhbq5jDh3Myb30yq7t5j5BtKTy0hvctn6cShnaMUjrDRLbXU6BK5vPbNcZ0l8K3l02V-bIe-t2XjQh-p52f6DjJJTP',
+            'H_BDCLCKID_SF_BFESS': 'tRAOoC--JKvqKRopMtOhq4tehHRkWT39WDTm_DoyBn8VHUjmQx452xufQR3TBh5DBe62-pPKKR7ADKoa5hCMKRLmDHrq3lTk3mkjbIbGfn02OPKz0TKVX-4syP4j2xRnWNTWKfA-b4ncjRcTehoM3xI8LNj405OTbIFO0KJDJCF5hDIwDjD-DjPVKgTa54cbb4o2WbCQKJTm8pcN2b5oQTtuKN-f-U5iQbruBhQxLPTWMtJ-XpOUWfAkXpJvQnJjt2JxaqRCyxFhbq5jDh3Myb30yq7t5j5BtKTy0hvctn6cShnaMUjrDRLbXU6BK5vPbNcZ0l8K3l02V-bIe-t2XjQh-p52f6DjJJTP',
+            'ppfuid': 'FOCoIC3q5fKa8fgJnwzbE67EJ49BGJeplOzf+4l4EOvDuu2RXBRv6R3A1AZMa49I27C0gDDLrJyxcIIeAeEhD8JYsoLTpBiaCXhLqvzbzmvy3SeAW17tKgNq/Xx+RgOdb8TWCFe62MVrDTY6lMf2GrfqL8c87KLF2qFER3obJGn/QJMAwAPnqrAi45EBm1RGGEimjy3MrXEpSuItnI4KD8bfH5NJAYbQDlO1QdCB1HdUs7xh/KYe4CwDQofIEpZ9w576stFR8hFaNkWJRUiuCBJsVwXkGdF24AsEQ3K5XBbh9EHAWDOg2T1ejpq0s2eFy9ar/j566XqWDobGoNNfmfpaEhZpob9le2b5QIEdiQez0E9SVndeXkd9EampG0PcXhLZ126CPFCIEuj/nWa+RAuJu9hkfHqEnTsCqvS3DivOXSxFWocn8LvXoXRLp3foHaFYUcWNK9vBxLWe1150xqUgehOYsWQjkg6YqniuoYPHZFO3SaB4GS7zlBrG2cLm8lTRl19JYcYcqvy3P/50mxpWDwUUC4pvKOF9e+pwNq7l6HzKEZyCMUDd+W6AiaksYiu+4AAz72OnMQfgAyNUbW3IyzL5c+UBht87WUigOY9alcIuR+n1gwn+Dmf3unATYGtv0zKmAog3Ny9wFYiQ/gdKSrR9D25HSwrLQyIe5QKTkKSlY6nVev8MhaT3AUPwNqYIvWCQZXWkhuuU0ZXLMYAKJSeHY7mTrwwSSKC3ZaIcOI0j9k46R3D6dNOT5eQrroOB3dqiBQpEuF9Iv7IX5ZK4hiE4AJ3OPZnO3aEJeMB91rSPWb2eeCt263/A+EJVR/A8+3BQ92SIDoXabq8Wb8ZGN9BAsC9g5OdjE6lhwzTadptHqT7mZN901gDzA4lMYEG/kekC+0J5/N5yVy+ei7UKhQHejRjxCO2+98Bn9oaYh/0qmUySClW5mphyLOoygh9eZkqFuuqiNXfbBwU2Yg+9JZ4edxM843Q5xndRbROGVYF8F1hSzrwF+N963ShNGO/mKL64uD0V1BPPvqVoeGBZrZdicvQj/Ixpg4HV9e/xxJNDJS+Dlsbqu3n4I65ukNGXLyEklC4yELuWd/LWcH9vINW2iNXxXOmtVELY74TxxJNDJS+Dlsbqu3n4I65uiZNjgrTAOu3w4UVXnTxXuL0FM/uFy3y/bm3bXiDfzlcmV/10jRpRMa1Wc01DNVRdz6tbH1m8PotlhxmYCxCuj53Swh7Kr1HWCzhMzIFEQ3X4I7OpaIP8n+Lzocjt3hpX',
+            'H_WISE_SIDS_BFESS': '60622_60677_60676_60672_60572',
+            'RT': '"z=1&dm=baidu.com&si=b82fd43a-4e99-46d0-b191-7856643c4174&ss=m0euuouv&sl=8&tt=77e&bcn=https%3A%2F%2Ffclog.baidu.com%2Flog%2Fweirwood%3Ftype%3Dperf&ld=1a15&ul=2m8v&hd=2m98"',
+            'M_LG_UID': '557804105',
+            'M_LG_SALT': 'a5fe20e3366a60db050a7756121c39a9',
+            'MCITY': '-%3A',
+            'validate': '25655',
+            'ab_sr': '1.0.1_NTNiZmVmNmQ3Yzc1MDNkYmM0ZjBjNWUyNWYxMzljOWRlY2YxNmM3YWRhMTU5YjMzM2Y2MGE0ZjE4MTE1OTAxOGEwZGI5ZDU5N2M1OWMyNmY1MmViZWZiNmUyY2ZlYmJmZmUwZmUwY2UzNmNhYjZmM2FkYjIxMjE3MWUyZjZjZWQ2N2U1ZjU2OGVmNTExYTIwYTYyOWFmNWE0Y2Q4NzhkZjQxZTg4N2ViZjFmOTY4ZjM3ZDZiMGJkYTQ0MTY1NzBh',
+        }
+
+        headers = {
+            'Accept': '*/*',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Acs-Token': '1724850536888_1724910423407_WRtG5z5UQccoQcFZSg82Ej9wJ5ADJz9igxYQdxeQjFMqAI9afiWsGqFBwpkt7sQTMqubcLcfMPRmCahJvU4U3/pNFVZeBgCQZdQw9WpUXMrL9jlOjUWgA6y0a0rkloTZ/kJuXVqz19E+ewHgHhZfmtWYSWjzGza0VUgeoGGokfeTHQq6C1828mBCNGvOPzoaq53Kjhdtc3BFafmEN6bl53VMNP+b20mZ2SzRRnCv63ECDVI9rVTwy9gJgkvlKGTP/j72BlumeJQbE1giErMHI4kPTE/yzDJaldZYQu82+v2FPvtXpVPHs8sVxrNSr5r6h7aKDKeK569MFWWVxADgyzxk0W+T8eDJWcbHa4fsJoZ0cvfb8aZRnoN7Utq/vyeoLG1YMAub3VvVRf2HhcqX3bcMMzPWA6zckOhFj/6Sz+1AZ1mJWUQeHfdixjMDEKTW3krh4m7/i58MplTHtA3L1goKlTlDpc1uV2qDb8rqdYOnk/pXKYt3Jf9EzVnfKmVO',
+            'Connection': 'keep-alive',
+            # 'Cookie': 'PSTM=1717486581; BIDUPSID=9C3C36BF544BED968833257BF4AB0604; BDUSS=ndXOFZqdGxET0M2cTRMV0lPU3VWa2ZRRmRWNWpnUUUxNWt6Zm5mYnBDYmtaWWRtRVFBQUFBJCQAAAAAAAAAAAEAAABJaj8heXgxODE5MzUxMTQwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAOTYX2bk2F9mc; BDUSS_BFESS=ndXOFZqdGxET0M2cTRMV0lPU3VWa2ZRRmRWNWpnUUUxNWt6Zm5mYnBDYmtaWWRtRVFBQUFBJCQAAAAAAAAAAAEAAABJaj8heXgxODE5MzUxMTQwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAOTYX2bk2F9mc; BAIDUID=F3EC97B6825FC9023AA6BFAF29C6FB7B:SL=0:NR=10:FG=1; H_WISE_SIDS=60622_60677_60676_60672_60572; BAIDUID_BFESS=F3EC97B6825FC9023AA6BFAF29C6FB7B:SL=0:NR=10:FG=1; ZFY=FCwsZLvf1:Bu5AvtfizlhFBTnYVuHltyCav:Bs015:ASgc:C; __bid_n=1917dcb1a5d843229a5c49; delPer=0; routetype=bus; H_PS_PSSID=60622_60677_60676_60672_60572_60684_60700; PSINO=1; BA_HECTOR=04ag2g2l80al2h8g202h8la42rr9nh1jd02bq1u; BDORZ=B490B5EBF6F3CD402E515D22BCDA1598; BCLID=11718421405245721845; BCLID_BFESS=11718421405245721845; BDSFRCVID=biPOJexroG3Qs37tFKW2MkFPPyx5pROTDYLEOwXPsp3LGJLVYZ59EG0PtHt5Ixub6j3eogKK0mOTHUkF_2uxOjjg8UtVJeC6EG0Ptf8g0M5; BDSFRCVID_BFESS=biPOJexroG3Qs37tFKW2MkFPPyx5pROTDYLEOwXPsp3LGJLVYZ59EG0PtHt5Ixub6j3eogKK0mOTHUkF_2uxOjjg8UtVJeC6EG0Ptf8g0M5; H_BDCLCKID_SF=tRAOoC--JKvqKRopMtOhq4tehHRkWT39WDTm_DoyBn8VHUjmQx452xufQR3TBh5DBe62-pPKKR7ADKoa5hCMKRLmDHrq3lTk3mkjbIbGfn02OPKz0TKVX-4syP4j2xRnWNTWKfA-b4ncjRcTehoM3xI8LNj405OTbIFO0KJDJCF5hDIwDjD-DjPVKgTa54cbb4o2WbCQKJTm8pcN2b5oQTtuKN-f-U5iQbruBhQxLPTWMtJ-XpOUWfAkXpJvQnJjt2JxaqRCyxFhbq5jDh3Myb30yq7t5j5BtKTy0hvctn6cShnaMUjrDRLbXU6BK5vPbNcZ0l8K3l02V-bIe-t2XjQh-p52f6DjJJTP; H_BDCLCKID_SF_BFESS=tRAOoC--JKvqKRopMtOhq4tehHRkWT39WDTm_DoyBn8VHUjmQx452xufQR3TBh5DBe62-pPKKR7ADKoa5hCMKRLmDHrq3lTk3mkjbIbGfn02OPKz0TKVX-4syP4j2xRnWNTWKfA-b4ncjRcTehoM3xI8LNj405OTbIFO0KJDJCF5hDIwDjD-DjPVKgTa54cbb4o2WbCQKJTm8pcN2b5oQTtuKN-f-U5iQbruBhQxLPTWMtJ-XpOUWfAkXpJvQnJjt2JxaqRCyxFhbq5jDh3Myb30yq7t5j5BtKTy0hvctn6cShnaMUjrDRLbXU6BK5vPbNcZ0l8K3l02V-bIe-t2XjQh-p52f6DjJJTP; ppfuid=FOCoIC3q5fKa8fgJnwzbE67EJ49BGJeplOzf+4l4EOvDuu2RXBRv6R3A1AZMa49I27C0gDDLrJyxcIIeAeEhD8JYsoLTpBiaCXhLqvzbzmvy3SeAW17tKgNq/Xx+RgOdb8TWCFe62MVrDTY6lMf2GrfqL8c87KLF2qFER3obJGn/QJMAwAPnqrAi45EBm1RGGEimjy3MrXEpSuItnI4KD8bfH5NJAYbQDlO1QdCB1HdUs7xh/KYe4CwDQofIEpZ9w576stFR8hFaNkWJRUiuCBJsVwXkGdF24AsEQ3K5XBbh9EHAWDOg2T1ejpq0s2eFy9ar/j566XqWDobGoNNfmfpaEhZpob9le2b5QIEdiQez0E9SVndeXkd9EampG0PcXhLZ126CPFCIEuj/nWa+RAuJu9hkfHqEnTsCqvS3DivOXSxFWocn8LvXoXRLp3foHaFYUcWNK9vBxLWe1150xqUgehOYsWQjkg6YqniuoYPHZFO3SaB4GS7zlBrG2cLm8lTRl19JYcYcqvy3P/50mxpWDwUUC4pvKOF9e+pwNq7l6HzKEZyCMUDd+W6AiaksYiu+4AAz72OnMQfgAyNUbW3IyzL5c+UBht87WUigOY9alcIuR+n1gwn+Dmf3unATYGtv0zKmAog3Ny9wFYiQ/gdKSrR9D25HSwrLQyIe5QKTkKSlY6nVev8MhaT3AUPwNqYIvWCQZXWkhuuU0ZXLMYAKJSeHY7mTrwwSSKC3ZaIcOI0j9k46R3D6dNOT5eQrroOB3dqiBQpEuF9Iv7IX5ZK4hiE4AJ3OPZnO3aEJeMB91rSPWb2eeCt263/A+EJVR/A8+3BQ92SIDoXabq8Wb8ZGN9BAsC9g5OdjE6lhwzTadptHqT7mZN901gDzA4lMYEG/kekC+0J5/N5yVy+ei7UKhQHejRjxCO2+98Bn9oaYh/0qmUySClW5mphyLOoygh9eZkqFuuqiNXfbBwU2Yg+9JZ4edxM843Q5xndRbROGVYF8F1hSzrwF+N963ShNGO/mKL64uD0V1BPPvqVoeGBZrZdicvQj/Ixpg4HV9e/xxJNDJS+Dlsbqu3n4I65ukNGXLyEklC4yELuWd/LWcH9vINW2iNXxXOmtVELY74TxxJNDJS+Dlsbqu3n4I65uiZNjgrTAOu3w4UVXnTxXuL0FM/uFy3y/bm3bXiDfzlcmV/10jRpRMa1Wc01DNVRdz6tbH1m8PotlhxmYCxCuj53Swh7Kr1HWCzhMzIFEQ3X4I7OpaIP8n+Lzocjt3hpX; H_WISE_SIDS_BFESS=60622_60677_60676_60672_60572; RT="z=1&dm=baidu.com&si=b82fd43a-4e99-46d0-b191-7856643c4174&ss=m0euuouv&sl=8&tt=77e&bcn=https%3A%2F%2Ffclog.baidu.com%2Flog%2Fweirwood%3Ftype%3Dperf&ld=1a15&ul=2m8v&hd=2m98"; M_LG_UID=557804105; M_LG_SALT=a5fe20e3366a60db050a7756121c39a9; MCITY=-%3A; validate=25655; ab_sr=1.0.1_NTNiZmVmNmQ3Yzc1MDNkYmM0ZjBjNWUyNWYxMzljOWRlY2YxNmM3YWRhMTU5YjMzM2Y2MGE0ZjE4MTE1OTAxOGEwZGI5ZDU5N2M1OWMyNmY1MmViZWZiNmUyY2ZlYmJmZmUwZmUwY2UzNmNhYjZmM2FkYjIxMjE3MWUyZjZjZWQ2N2U1ZjU2OGVmNTExYTIwYTYyOWFmNWE0Y2Q4NzhkZjQxZTg4N2ViZjFmOTY4ZjM3ZDZiMGJkYTQ0MTY1NzBh',
+            'Referer': 'https://map.baidu.com/@11884636.125,3435440,19z',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
+            'sec-ch-ua': '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+        }
+        # address = '山东省威海市经济技术开发区黄岛路-1-1号'
+        response = requests.get(
+            'https://map.baidu.com/?newmap=1&reqflag=pcmap&biz=1&from=webmap&da_par=direct&pcevaname=pc4.1&qt=s&da_src=searchBox.button&wd=替换地址&c=132&src=0&wd2=&pn=0&sug=0&l=19&b=(11884316.125,3435300.25;11884956.125,3435579.75)&from=webmap&biz_forward={%22scaler%22:2,%22styles%22:%22pl%22}&sug_forward=&auth=RTG4VWNbXW7U54%3D54c0V1wUDFJ0yNd00uxNzETxVEzHt2OF%3D2UUF9FDFCwyS8v7uvkGcuVtvvhguVtvyheuVtvCMGuVtvCQMuVtvIPcuVtvYvjuVtvZgMuVtv%40vcuVtvc3CuVtcvY1SGpuxBt2o88GdFveGvuVtveh3uxtwiKDv7uvhgMuxVVtvrMhuVtGccZcuxtf0wd0vyOAFUy7FAI&seckey=C5HY%2F2XzySHqt3BGtBTgM8RTNLhRAr3YvXAa0s3daZ8%3D%2CfjvWt4Zn9rZe_NRN5j6ZlpP7jeQx1LC84Y7Y2EeJuChO59IiRZn38zYABmOdFppcKZlJLlIPF_BwvglEwW-Sc9HYMLxI5Jn59KjpTKBVsf8NWid8HbLv2D7hhbFEEfjMUyqwp5WD4X22ORF3jQTnp8FcYwO7QcZfy3LPUrYFVUYnDQr89lHAblVZN2T1EWeS&device_ratio=2&tn=B_NORMAL_MAP&nn=0&u_loc=12964711,4826154&ie=utf-8&t=1724910423385&newfrom=zhuzhan_webmap'.replace(
+                '替换地址', address),
+            cookies=cookies,
+            headers=headers,
+        )
+        province_name = response.json()['content'][0]['admin_info']['province_name']
+        city_name = response.json()['content'][0]['admin_info']['city_name']
+        area_name = response.json()['content'][0]['admin_info']['area_name']
+        area_id = response.json()['content'][0]['admin_info']['area_id']
+        return f'{province_name}{city_name}{area_name}', area_id
+    except:
+        return '', ''
+
+
+def get_data():
+    db_doris = pymysql.connect(host="10.32.48.2", user="z_smy", password="z_smy@0325", database="pro_qd_ztjc", charset="utf8")
+    cursor_doris = db_doris.cursor()
+    cursor_doris.execute("""SELECT 
+	id,
+	case 
+	when label = '迁入' THEN add_before
+	else add_after end
+	address
+FROM `pro_qd_dp_list_migrateinout` where c1=''""")
+    data_list = cursor_doris.fetchall()
+    for data in data_list:
+        id = data[0]
+        address = data[1]
+        c1, c2 = get_address_info(address.replace('(', '').replace(')', '')[:20])
+        print(id, address, c1, c2)
+        sql = f"update pro_qd_dp_list_migrateinout set c1='{c1}', c2='{c2}' where id={id}"
+        # print(sql)
+        cursor_doris.execute(sql)
+        db_doris.commit()
+
+
+if __name__ == '__main__':
+    # get_data()
+    print(get_address_info1('山东省青岛市保税港区上海路34号三段五层5171（B） '))
--- scripts/news_spider.py
+++ scripts/news_spider.py
@@ -1,15 +1,10 @@
-# -*- coding:UTF-8 -*-
-# @Time    : 24.9.20 15:45
-# @Author  : yangxin
-# @Email   : yang1819351140@163.com
-# @IDE     : PyCharm
-# @File    : news_spider.py
-# @Project : pythonScript
-# @Software: PyCharm
 import requests
-import redis
+import hashlib
+import json
 import random
-from lxml import etree
+from time import sleep
+import redis
+import requests
 from gne import GeneralNewsExtractor
 
 r = redis.Redis(connection_pool=redis.ConnectionPool(host="10.32.51.2", port=6379, db=15, decode_responses=True))
@@ -21,72 +16,219 @@ def get_proxies():
     return {"http": f'http://{ip}', "https": f'http://{ip}'}
 
 
-def get_url_list(keyword):
-    # # html页面取数据
-    # url = f'https://www.bjnews.com.cn/search?bwsk={keyword}'
-    # headers = {
-    #     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    #     "Accept-Language": "en",
-    #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'
-    # }
-    # res = requests.get(url, headers=headers, proxies=get_proxies())
-    # html = etree.HTML(res.text)
-    # url_list = html.xpath('//h3[@class="articleTitle"]/a/@href')
-    # for url in url_list:
-    #     try:
-    #         get_content(url)
-    #     except Exception as e:
-    #         print(e)
-    #         continue
+class NewsSpider(object):
+    # 主题词
+    keywords = ["儿童意外伤害", "儿童忽视"]
+    # 细分主题词：儿童
+    child_keywords = ["孩子", "儿童", "女孩", "男孩", "女童", "男童", "孤儿", "幼儿", "小学生", "中学生", "高中生",
+                      "1岁", "2岁", "3岁", "4岁", "5岁", "6岁", "7岁", "8岁", "9岁", "10岁", "11岁", "12岁", "13岁",
+                      "14岁", "15岁", "16岁", "17岁", "不满18岁"]
+    # 细分主题词：伤害程度
+    injury_severity_keywords = ["无受伤", "轻度受伤", "中度受伤", "重度受伤", "死亡", "失踪", "重伤", "轻伤"]
+    # 细分主题词：伤害类型
+    injury_type_keywords = ["被拐", "烫伤", "烧伤", "掉水里", "掉河里", "溺水", "溺亡", "车祸", "事故", "交通事故",
+                            "吞食", "不小心", "一不留神", "走失", "坠楼", "坠落", "强奸", "强制猥亵", "被打", "跌落",
+                            "摔伤", "电器", "电击", "被电", "磕碰", "割伤", "划伤", "意外伤害", "意外受伤", "玩火",
+                            "放炮"]
 
-    # json接口取数据
-    url = f'https://s.bjnews.com.cn/bjnews/getlist?from=bw&page=2&orderby=0&bwsk={keyword}'
-    headers = {
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Accept-Language": "en",
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'
-    }
-    res = requests.get(url, headers=headers, proxies=get_proxies())
-    data_list = res.json().get('data', {}).get('data', [])
-    if data_list:
-        for data in data_list[:2]:
+    def __init__(self):
+        self.bjnews = open("bjnews.txt", "a", encoding="UTF-8")
+        self.thepaper = open("thepaper.txt", "a", encoding="UTF-8")
+        self.infzm = open("infzm.txt", "a", encoding="UTF-8")
+        self.proxies = get_proxies()
+        self.news_hash = {}
+
+    def bjnews_spider(self, keyword):
+        print(keyword)
+        page = 1
+        while 1:
+            # json接口取数据
+            url = f'https://s.bjnews.com.cn/bjnews/getlist?from=bw&page={page}&orderby=0&bwsk={keyword}'
+            res = self.get_response(url)
+            data = res.json().get('data', {})
+            if not data:
+                break
+            data_list = data.get('data', [])
+            if data_list:
+                for data in data_list:
+                    try:
+                        detail_url = data.get('_source', {}).get('detail_url', {}).get('pc_url', '')
+                        publish_time = data.get('_source', {}).get('publish_time', '')
+                        title = data.get('_source', {}).get('title', '')
+                        content = data.get('highlight', {}).get('content', '')
+                        title_hash = hashlib.md5(title.encode("UTF-8")).hexdigest()
+                        if not r.sismember('title_hash', title_hash):
+                            data = {
+                                'keyword': keyword,
+                                'title': title,
+                                'publish_time': publish_time,
+                                'content': content,
+                                'url': detail_url
+                            }
+                            print(data)
+                            self.bjnews.write(json.dumps(data, ensure_ascii=False) + '\n')
+                            r.sadd('title_hash', title_hash)
+                    except Exception as e:
+                        print(e)
+                page += 1
+                print(f'{url} 采集完毕')
+            else:
+                break
+
+    def thepaper_spider(self, keyword):
+        print(keyword)
+        page = 1
+        while 1:
+            # json接口取数据
+            url = f'https://api.thepaper.cn/search/web/news'
+            data = {"word": keyword, "orderType": 3, "pageNum": page, "pageSize": 10, "searchType": 1}
+            res = self.get_response(url, data=data)
+            data_list = res.json().get('data', {}).get('list', [])
+            if not data_list:
+                break
+            for data in data_list:
+                try:
+                    news_id = data.get('contId', '')
+                    if news_id and not r.sismember('title_hash', f'thepaper_{news_id}'):
+                        self.get_thepaper_content(news_id, keyword)
+                        r.sadd('title_hash', f'thepaper_{news_id}')
+                except Exception as e:
+                    print(e)
+            page += 1
+            print(f'{url} 采集完毕')
+
+    def get_thepaper_content(self, news_id, keyword):
+        try:
+            url = f'https://www.thepaper.cn/newsDetail_forward_{news_id}'
+            res = self.get_response(url)
+            extractor = GeneralNewsExtractor()
+            extract_result = extractor.extract(html=res.text, with_body_html=True)
+            # 新闻作者
+            news_author = extract_result['author']
+            # 新闻发布时间
+            news_publish_time = extract_result['publish_time']
+            # 新闻标题
+            news_title_gne = extract_result['title']
+            # 新闻内容
+            news_content_gne = extract_result['content']
+            # 新闻内容 HTML
+            news_content_html_gne = extract_result['body_html']
+
+            data = {
+                'keyword': keyword,
+                'title': news_title_gne,
+                'author': news_author,
+                'publish_time': news_publish_time,
+                'content': news_content_gne,
+                'content_html': news_content_html_gne
+            }
+            # 数据写入文件
+            print(data)
+            self.thepaper.write(json.dumps(data, ensure_ascii=False) + '\n')
+        except Exception as e:
+            print(f'[get_thepaper_content]{e}')
+
+    def infzm_spider(self, keyword):
+        print(keyword)
+        page = 1
+        while 1:
+            # json接口取数据
+            url = f'https://www.infzm.com/search?term_id=&page={page}&k={keyword}&format=json'
+            res = self.get_response(url)
+            data_list = res.json().get('data', {}).get('list', [])
+            if not data_list:
+                break
+            for data in data_list:
+                try:
+                    news_id = data.get('id', '')
+                    if news_id and not r.sismember('title_hash', f'infzm_{news_id}'):
+                        self.get_infzm_content(news_id, keyword)
+                        r.sadd('title_hash', f'infzm_{news_id}')
+                except Exception as e:
+                    print(e)
+            page += 1
+            print(f'{url} 采集完毕')
+
+    def get_infzm_content(self, news_id, keyword):
+        try:
+            url = f'https://www.infzm.com/contents/{news_id}'
+            res = self.get_response(url)
+            extractor = GeneralNewsExtractor()
+            extract_result = extractor.extract(html=res.text, with_body_html=True)
+            # 新闻作者
+            news_author = extract_result['author']
+            # 新闻发布时间
+            news_publish_time = extract_result['publish_time']
+            # 新闻标题
+            news_title_gne = extract_result['title']
+            # 新闻内容
+            news_content_gne = extract_result['content']
+            # 新闻内容 HTML
+            news_content_html_gne = extract_result['body_html']
+
+            data = {
+                'keyword': keyword,
+                'title': news_title_gne,
+                'author': news_author,
+                'publish_time': news_publish_time,
+                'content': news_content_gne,
+                'content_html': news_content_html_gne
+            }
+            # 数据写入文件
+            print(data)
+            self.infzm.write(json.dumps(data, ensure_ascii=False) + '\n')
+        except Exception as e:
+            print(f'[get_infzm_content]{e}')
+
+    def get_response(self, url, data=None):
+        while 1:
+            headers = {
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                "Accept-Language": "en",
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'
+            }
             try:
-                url = data.get('_source', {}).get('detail_url', {}).get('pc_url', '')
-                get_content(url)
+                if not data:
+                    res = requests.get(url, headers=headers, proxies=self.proxies, timeout=10)
+                else:
+                    res = requests.post(url, json=data, headers=headers, proxies=self.proxies, timeout=10)
+                if res.status_code == 200:
+                    return res
+                else:
+                    new_proxy = get_proxies()
+                    print(f'{self.proxies} 已失效, 更换代理 {new_proxy}')
+                    self.proxies = get_proxies()
             except Exception as e:
                 print(e)
+                new_proxy = get_proxies()
+                print(f'{self.proxies} 已失效, 更换代理 {new_proxy}')
+                self.proxies = get_proxies()
 
+    def __del__(self):
+        self.bjnews.close()
+        self.thepaper.close()
+        self.infzm.close()
 
-def get_content(url):
-    headers = {
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Accept-Language": "en",
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'
-    }
-    res = requests.get(url, headers=headers, proxies=get_proxies())
-    extractor = GeneralNewsExtractor()
-    extract_result = extractor.extract(html=res.text, with_body_html=True)
-    # 新闻作者
-    news_author = extract_result['author']
-    # 新闻发布时间
-    news_publish_time = extract_result['publish_time']
-    # 新闻标题
-    news_title_gne = extract_result['title']
-    # 新闻内容
-    news_content_gne = extract_result['content']
-    # 新闻内容 HTML
-    news_content_html_gne = extract_result['body_html']
-    data = {
-        'title': news_title_gne,
-        'author': news_author,
-        'publish_time': news_publish_time,
-        'content': news_content_gne,
-        'content_html': news_content_html_gne,
-    }
-    print(data)
+    def get_content(self, keyword):
+        self.bjnews_spider(keyword)
+        # self.thepaper_spider(keyword)
+        # self.infzm_spider(keyword)
+
+    def run(self):
+        for keyword in self.keywords:
+            self.get_content(keyword)
+            sleep(2)
+            for child_keyword in self.child_keywords:
+                self.get_content(f'{keyword}%20{child_keyword}')
+                sleep(2)
+                for injury_severity_keyword in self.injury_severity_keywords:
+                    self.get_content(f'{keyword}%20{child_keyword}%20{injury_severity_keyword}')
+                    sleep(2)
+                for injury_type_keyword in self.injury_type_keywords:
+                    self.get_content(f'{keyword}%20{child_keyword}%20{injury_type_keyword}')
+                    sleep(2)
 
 
 if __name__ == '__main__':
-    keyword = '汽车'
-    get_url_list(keyword)
-
+    news_spider = NewsSpider()
+    news_spider.run()
